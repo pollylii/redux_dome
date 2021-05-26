e62dc409d634d4b711b74642af9d7e7db5497dff
pollylilh <794211268@qq.com>
2021-05-26 17:41:53 +0800

    react-redux基本使用

--- src/components/Count/index.jsx
+++ src/components/Count/index.jsx
@@ -1,54 +1,37 @@
 import React, { Component } from 'react'
-//引入store，用于获取redux中保存状态
-import store from '../../redux/store'
-//引入actionCreator，专门用于创建action对象
-import {
-	createIncrementAction,
-	createDecrementAction,
-	createIncrementAsyncAction
-} from '../../redux/count_action'
 
 export default class Count extends Component {
 
 	state = {carName:'奔驰c63'}
 
-	/* componentDidMount(){
-		//检测redux中状态的变化，只要变化，就调用render
-		store.subscribe(()=>{
-			this.setState({})
-		})
-	} */
-
 	//加法
 	increment = ()=>{
 		const {value} = this.selectNumber
-		store.dispatch(createIncrementAction(value*1))
+		this.props.jia(value*1)
 	}
 	//减法
 	decrement = ()=>{
 		const {value} = this.selectNumber
-		store.dispatch(createDecrementAction(value*1))
+		this.props.jian(value*1)
 	}
 	//奇数再加
 	incrementIfOdd = ()=>{
 		const {value} = this.selectNumber
-		const count = store.getState()
-		if(count % 2 !== 0){
-			store.dispatch(createIncrementAction(value*1))
+		if(this.props.count % 2 !== 0){
+			this.props.jia(value*1)
 		}
 	}
 	//异步加
 	incrementAsync = ()=>{
 		const {value} = this.selectNumber
-		// setTimeout(()=>{
-			store.dispatch(createIncrementAsyncAction(value*1,500))
-		// },500)
+		this.props.jiaAsync(value*1,500)
 	}
 
 	render() {
+		//console.log('UI组件接收到的props是',this.props);
 		return (
 			<div>
-				<h1>当前求和为：{store.getState()}</h1>
+				<h1>当前求和为：{this.props.count}</h1>
 				<select ref={c => this.selectNumber = c}>
 					<option value="1">1</option>
 					<option value="2">2</option>
