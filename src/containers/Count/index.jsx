b00051cdf055540b6f90a6a184d567c539e5f8fb
pollylilh <794211268@qq.com>
2021-06-18 14:07:23 +0800

    组件数据

--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -4,7 +4,7 @@ import {
 	createIncrementAction,
 	createDecrementAction,
 	createIncrementAsyncAction
-} from '../../redux/count_action'
+} from '../../redux/actions/count'
 //引入connect用于连接UI组件与redux
 import {connect} from 'react-redux'
 
@@ -40,7 +40,8 @@ class Count extends Component {
 		//console.log('UI组件接收到的props是',this.props);
 		return (
 			<div>
-				<h1>当前求和为：{this.props.count}</h1>
+				<h2>我是Count组件,下方组件总人数为:{this.props.renshu}</h2>
+				<h4>当前求和为：{this.props.count}</h4>
 				<select ref={c => this.selectNumber = c}>
 					<option value="1">1</option>
 					<option value="2">2</option>
@@ -57,16 +58,12 @@ class Count extends Component {
 
 //使用connect()()创建并暴露一个Count的容器组件
 export default connect(
-	state => ({count:state}),
-
-	//mapDispatchToProps的一般写法
-	/* dispatch => ({
-		jia:number => dispatch(createIncrementAction(number)),
-		jian:number => dispatch(createDecrementAction(number)),
-		jiaAsync:(number,time) => dispatch(createIncrementAsyncAction(number,time)),
-	}) */
-
-	//mapDispatchToProps的简写
+	// 映射状态
+	state => ({
+		count:state.he,
+		renshu:state.rens.length
+	}),
+	// 映射操作状态的方法
 	{
 		jia:createIncrementAction,
 		jian:createDecrementAction,
--- src/index.js
+++ src/index.js
@@ -10,8 +10,3 @@ ReactDOM.render(
 	</Provider>,
 	document.getElementById('root')
 )
-
-//监测redux中状态的改变，如redux的状态发生了改变，那么重新渲染App组件
-// store.subscribe(()=>{
-// 	ReactDOM.render(<App/>,document.getElementById('root'))
-// })
